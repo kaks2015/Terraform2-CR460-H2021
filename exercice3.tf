4f68b55acb22dd1ec7e0ec270699888a7215a58c
CloudGuru2021 <mkakoro604@gmail.com>
2021-02-13 20:49:47 -0500

    infra1
    ceci est le commit de la première infrastructure as code que je déploie

--- exercice3.tf
+++ exercice3.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  project = "zonedetest"
+  project = "cr460-dev1-304419"
   credentials = "account.json"
   region  = "northamerica-northeast1"
   zone    = "northamerica-northeast1-a"
@@ -29,7 +29,8 @@ resource "google_compute_instance" "instance2" {
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-1910"
+      #image = "ubuntu-os-cloud/ubuntu-1910"
+      image = "ubuntu-os-cloud/ubuntu-1804-lts"
     }
   }
 
